4d743d84fc7a404f1df2ee19093a50f3c9db2577
Hafeez Ghanchi <hafeezghanchi927@gmail.com>
2021-03-31 05:23:52 +0500

    created user and added relationship

--- src/models.py
+++ src/models.py
@@ -1,4 +1,18 @@
-from src import db;
+from sqlalchemy.orm import backref, lazyload
+from src import db
+
+# creating table for users
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(length=30), nullable=False, unique=True)
+    email = db.Column(db.String(length=50), nullable=False, unique=True)
+    password = db.Column(db.String(length=60), nullable=False)
+    budget = db.Column(db.Integer(), nullable=False, default=1000)
+    item = db.relationship('Item', backref='owned_user', lazy=True)
+
+
+    def __repr__(self):
+        return self.username
 
 # creating table for products
 class Item(db.Model):
@@ -6,8 +20,8 @@ class Item(db.Model):
     name = db.Column(db.String(length=30), nullable=False, unique=True)
     price = db.Column(db.Integer(), nullable=False)
     barcode = db.Column(db.String(length=8), nullable=False, unique=True)
-    description = db.Column(db.String(length=1024),
-                            nullable=False, unique=True)
+    description = db.Column(db.String(length=1024), nullable=False, unique=True)
+    owner = db.Column(db.Integer(),db.ForeignKey('user.id'))
 
     def __repr__(self):
         return self.name
