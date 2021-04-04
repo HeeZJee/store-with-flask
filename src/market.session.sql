a3b8017479c5f2e8717b1a15c6f2fc4d6093e7b1
Hafeez Ghanchi <hafeezghanchi927@gmail.com>
2021-04-04 17:32:21 +0500

    updated db configs

--- src/market.session.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO item (id, name, price, barcode, description)
-VALUES (
-    id:INTEGER,
-    'name:VARCHAR(30)',
-    price:INTEGER,
-    'barcode:VARCHAR(8)',
-    'description:VARCHAR(1024)'
-  );
-  
