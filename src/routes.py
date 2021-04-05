ba7070eb90f42e03e8cf9d465c1a0468f29b3f36
Hafeez Ghanchi <hafeezghanchi927@gmail.com>
2021-04-06 04:48:37 +0500

    auto login on registration

--- src/routes.py
+++ src/routes.py
@@ -4,7 +4,7 @@ from typing import List
 from src.models import Item, User
 from src.forms import LoginForm, RegisterForm
 from src import db
-from flask_login import login_user, login_required
+from flask_login import login_user, login_required, logout_user
 
 # adding route for home page
 @app.route('/')
@@ -19,13 +19,8 @@ def index():
 def market():
 
     items: List[object] = Item.query.all()
-    # items: List[object] = [
-    #     {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
-    #     {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
-    #     {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
-    # ]
+
     return render_template("market.html", items=items)
-# adding route for registration page
 
 
 # adding route for registration page
@@ -38,8 +33,7 @@ def login():
 
         if attempted_user and attempted_user.check_password_correction(attempted_password=form.password.data):
             login_user(attempted_user)
-            flash(
-                f"Success! you are logged in as {attempted_user.username}", category='success')
+            flash(f"Success! you are logged in as {attempted_user.username}", category='success')
             return redirect(url_for('market'))
         else:
             flash("Username and Password are not match please try again.",
@@ -50,7 +44,7 @@ def login():
 @app.route('/register',methods=['GET',"POST"])
 def register():
 
-    # create instance of form
+    # create instance registration of form
     form = RegisterForm()
 
     #  adding data to database on clicking submit button
@@ -63,6 +57,10 @@ def register():
 
         db.session.add(user_to_create)
         db.session.commit()
+        
+        login_user(user_to_create)
+        flash(f"Successfully registered! you are logged in as {user_to_create.username}", category='success')
+
         return redirect(url_for('market'))
 
     #  checking for errors on creating user
